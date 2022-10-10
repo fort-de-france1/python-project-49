f1e3cdcb900864a3a38446e8e2deb9ec1c559bc6
hex-learning <leontiytsagoiko@gmail.com>
2022-10-10 12:13:31 +0300

    step 4

--- Makefile
+++ Makefile
@@ -8,4 +8,5 @@ publish:
 	poetry publish --dry-run
 package-install:
 	python3 -m pip install --user dist/hexlet_code-0.1.0-py3-none-any.whl
-
+lint:
+	poetry run flake8 brain_games
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,8 +3,9 @@ from brain_games import cli
 
 
 def main():
-	print("Welcome to the Brain Games!")
-	cli.welcome_user()
-if __name__ == "__main__":
-	main()
+    print("Welcome to the Brain Games!")
+    cli.welcome_user()
 
+
+if __name__ == "__main__":
+    main()
